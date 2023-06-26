e619c056c5a7d7f332489a06988708c01209e977
Rafael Ferraro <rafael.m.ferraro@gmail.com>
2023-06-26 09:25:50 -0400

    adding fixture to determine cache status for tests only

--- tests/unit/test_handler.py
+++ tests/unit/test_handler.py
@@ -1,5 +1,6 @@
 import os
 
+import boto3
 import pytest
 
 from src.lambda_functions.get_naip_tile import _get_tile_server_config, handler
@@ -11,19 +12,28 @@ def tile_server_config():
     return TileServerConfig.from_env()
 
 
+@pytest.fixture(scope="module")
+def cache_enabled(tile_server_config):
+    cache_s3_bucket = boto3.resource("s3").Bucket(tile_server_config.tile_cache_bucket)
+    return False if not cache_s3_bucket.creation_date else True
+
+
 def test_tile_image_with_naip_coverage_via_event():
     """Test confirms Lambda handler (via event) returns valid image for a tile with NAIP coverage."""
     result = handler({"x": 425, "y": 776, "z": 11, "year": 2021}, {})
     assert result["statusCode"] == 200
 
 
-def test_tile_image_without_naip_coverage_via_event(helpers):
+def test_tile_image_without_naip_coverage_via_event(helpers, cache_enabled):
     """Test confirms Lambda handler (via event) returns blank tile image for a tile without NAIP coverage."""
     result = handler({"x": 776, "y": 425, "z": 11, "year": 2021}, {})
-    assert result["statusCode"] == 200
-    tile_image = helpers.decode_b64_image(result["body"])
-    assert tile_image is not None
-    assert helpers.is_blank_image(tile_image)
+    if cache_enabled:
+        assert result["statusCode"] == 200
+        tile_image = helpers.decode_b64_image(result["body"])
+        assert tile_image is not None
+        assert helpers.is_blank_image(tile_image)
+    else:
+        assert result["statusCode"] == 404
 
 
 def test_tile_image_with_naip_coverage_via_path_params():
@@ -33,14 +43,17 @@ def test_tile_image_with_naip_coverage_via_path_params():
     assert result["statusCode"] == 200
 
 
-def test_tile_image_without_naip_coverage_via_path_params(helpers):
+def test_tile_image_without_naip_coverage_via_path_params(helpers, cache_enabled):
     """Test confirms Lambda handler (via path params) returns blank tile image for a tile without NAIP coverage."""
     event = {"pathParameters": {"x": 776, "y": 425, "z": 11, "year": 2021}}
     result = handler(event, {})
-    assert result["statusCode"] == 200
-    tile_image = helpers.decode_b64_image(result["body"])
-    assert tile_image is not None
-    assert helpers.is_blank_image(tile_image)
+    if cache_enabled:
+        assert result["statusCode"] == 200
+        tile_image = helpers.decode_b64_image(result["body"])
+        assert tile_image is not None
+        assert helpers.is_blank_image(tile_image)
+    else:
+        assert result["statusCode"] == 404
 
 
 def test_missing_parameters():
