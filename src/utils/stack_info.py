2c7c34b05b895fb9ed46a0fdb3913e327edf9462
Rafael Ferraro <rafael.m.ferraro@gmail.com>
2023-06-26 09:28:08 -0400

    refactor

--- src/utils/stack_info.py
+++ src/utils/stack_info.py
@@ -7,21 +7,31 @@ import tomli
 
 
 @lru_cache(maxsize=1)
-def _get_stack_outputs():
-    with open(os.path.join(Path(__file__).parent.parent.parent, "samconfig.toml"), mode="rb") as fp:
-        config = tomli.load(fp)
-        stack_name = config["default"]["global"]["parameters"]["stack_name"]
-
+def get_stack_description():
+    """Get info about the deployed CloudFormation."""
+    stack_name = get_stack_name()
     client = boto3.client("cloudformation")
     try:
         response = client.describe_stacks(StackName=stack_name)
-        stacks = response["Stacks"]
-        return stacks[0]["Outputs"]
+        return response["Stacks"][0]
 
     except Exception as e:
         raise ValueError(
             f"Cannot find stack {stack_name} \n" f'Please make sure a stack with the name "{stack_name}" exists'
         ) from e
+
+
+def get_stack_name() -> str:
+    """Get the stack name per samconfig.toml.
+
+    Returns
+    -------
+    str
+    """
+    with open(os.path.join(Path(__file__).parent.parent.parent, "samconfig.toml"), mode="rb") as fp:
+        config = tomli.load(fp)
+        stack_name = config["default"]["global"]["parameters"]["stack_name"]
+    return stack_name
 
 
 def get_stack_output_value(output_key: str) -> str:
@@ -37,7 +47,7 @@ def get_stack_output_value(output_key: str) -> str:
     str:
         key's value if found,
     """
-    stack_outputs = _get_stack_outputs()
+    stack_outputs = get_stack_description()["Outputs"]
     api_outputs = [output for output in stack_outputs if output["OutputKey"] == output_key]
 
     if not api_outputs:
@@ -56,14 +66,14 @@ def get_is_stack_deployed() -> bool:
 
     """
     try:
-        _get_stack_outputs()
+        get_stack_description()
         return True
     except Exception:
         return False
 
 
 def get_is_cache_enabled() -> bool:
-    """Crude check to determine if tile cacheing is enabled in current configuration.
+    """Crude check to determine if tile caching is enabled in current stack configuration.
 
     CloudFormation parameters are not allowed to have null/empty value - so its possible some dummy values is used
     for NAIPTileCacheS3Bucket Parameter (i.e. S3 bucket is non-existent).  It's also possible that a once valid bucket
